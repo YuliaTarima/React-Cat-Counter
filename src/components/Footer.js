0c0d51854425ff89fb81952362f204b9d5afc107
Yulia <yulia.tarima@gmail.com>
2020-02-07 20:18:25 -0800

    Edit: added owner link to Footer copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,16 @@ function Footer() {
   return (
     <div className="container p-3 my-3 bg-primary text-white border">
       <footer>
-        &copy; YuWebdesign {new Date().getFullYear()} - 2021
+        &copy;
+        <a
+           className="App-link"
+           href="YuWebdesign"
+           target="_blank"
+           rel="noopener noreferrer"
+        >
+        YuWebdesign&nbsp;
+        </a>
+        {new Date().getFullYear()} - 2021
       </footer>
     </div>
   );
