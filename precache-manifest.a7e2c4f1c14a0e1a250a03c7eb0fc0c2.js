a98ff050d7bd9a3c7e0d1b891e241d5abaf59745
Yulia <yulia.tarima@gmail.com>
2020-02-21 16:42:08 -0800

    Updates

---
+++ precache-manifest.a7e2c4f1c14a0e1a250a03c7eb0fc0c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37de82503170649c6f16a7cb497e3e69",
+    "url": "/React-Cat-Counter/index.html"
+  },
+  {
+    "revision": "be936094a2251c4ba269",
+    "url": "/React-Cat-Counter/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "45607fec812bd08ecd65",
+    "url": "/React-Cat-Counter/static/css/main.01717d17.chunk.css"
+  },
+  {
+    "revision": "be936094a2251c4ba269",
+    "url": "/React-Cat-Counter/static/js/2.89c55a8c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Cat-Counter/static/js/2.89c55a8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45607fec812bd08ecd65",
+    "url": "/React-Cat-Counter/static/js/main.8a9736ce.chunk.js"
+  },
+  {
+    "revision": "8b46d282efbc4bccec4a",
+    "url": "/React-Cat-Counter/static/js/runtime-main.24b0d8fe.js"
+  },
+  {
+    "revision": "56688d3d9138d38a7fc399294efa6060",
+    "url": "/React-Cat-Counter/static/media/logo.56688d3d.jpg"
+  }
+]);
