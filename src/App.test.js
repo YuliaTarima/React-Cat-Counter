b10da73902db161ea9c5ac879eb4d63e347bcbb1
Yulia <yulia.tarima@gmail.com>
2020-02-29 21:29:13 -0800

    WIP: circleCI configuration

--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+test('renders without crashing', () => {
+    shallow(<App />);
 });
