b026c26051f2d88e6911940434503184a6496f6b
Yulia Tarima <yulia.tarima@gmail.com>
2020-03-11 12:58:59 -0700

    WIP: added <Provider /> in index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import store from '././redux/store';
+import {Provider} from 'react-redux';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
